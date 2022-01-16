ebabe5e8edebe79cc32ffe2dfce9b385037b61c3
Franco Cusmai <fcusmai@b2gov.com>
2022-01-16 18:00:36 -0300

    vue add i18n

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,17 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <Hello/>
   </div>
 </template>
+
+
+<script>
+import Hello from '@/components/HelloI18n'
+
+export default {
+  components: {
+    Hello
+  }
+}
+</script>
