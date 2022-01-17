7bb50d842c95f5b6694b0514d9a8a6bba1aca13d
Franco Cusmai <fcusmai@b2gov.com>
2022-01-17 18:04:18 -0300

    responsive

--- src/components/TheFooter.vue
+++ src/components/TheFooter.vue
@@ -1,34 +1,77 @@
 <template>
-    <b-navbar variant="dark" class="px-lg-5 px-3 d-flex justify-content-between">
-        <b-nav>
-            <b-nav-item>
-                <b-icon variant="white-2" icon="instagram"></b-icon>
-            </b-nav-item>
-            <b-nav-item>
-                <b-icon variant="white-2" icon="youtube"></b-icon>
-            </b-nav-item>
-            <b-nav-item>
-                <b-icon variant="white-2" icon="twitter"></b-icon>
-            </b-nav-item>
-        </b-nav>
+    <div>
+        <div class="px-lg-5 px-3 d-none d-sm-flex justify-content-between">
+            <b-nav class="my-auto">
+                <b-nav-item>
+                    <b-icon icon="instagram"></b-icon>
+                </b-nav-item>
+                <b-nav-item>
+                    <b-icon icon="youtube"></b-icon>
+                </b-nav-item>
+                <b-nav-item>
+                    <b-icon icon="twitter"></b-icon>
+                </b-nav-item>
+            </b-nav>
 
-        <b-nav>
-            <b-navbar-brand>© 2022 Unnamed Item Studios</b-navbar-brand>
-        </b-nav>
+            <b-nav>
+                <b-navbar-brand>© 2022 Unnamed Item Studios</b-navbar-brand>
+            </b-nav>
 
-        <b-nav>
-            <b-nav-item-dropdown dropup>
-                <template #button-content>
-                    Languaje
-                </template>
+            <b-nav class="d-flex justify-content-end">
+                <b-nav-item-dropdown :text="$t('settings')" dropup>
+                    <b-dropdown-text>{{$t('languaje')}}</b-dropdown-text>
 
-                <b-dropdown-item>A</b-dropdown-item>
-                <b-dropdown-item>A</b-dropdown-item>
-                <b-dropdown-item>A</b-dropdown-item>
-                <b-dropdown-item>A</b-dropdown-item>
-            </b-nav-item-dropdown>
-        </b-nav>
-    </b-navbar>
+                    <b-dropdown-divider></b-dropdown-divider>
+
+                    <b-dropdown-item @click="$i18n.locale = 'es'">
+                        <span><img src="@/assets/flags/es.png" width="30" height="18"> {{$t('spanish')}}</span>
+                    </b-dropdown-item>
+                    <b-dropdown-item @click="$i18n.locale = 'en'">
+                        <span><img src="@/assets/flags/uk.png" width="30" height="18"> {{$t('english')}}</span>
+                    </b-dropdown-item>
+
+                    <b-dropdown-divider></b-dropdown-divider>
+
+                    <b-dropdown-item @click="$theme.switch()">
+                        <b-icon :icon="$theme.color == $theme.dark ? 'moon' : 'sun'"></b-icon> {{$t($theme.color)}}
+                    </b-dropdown-item>
+                </b-nav-item-dropdown>
+            </b-nav>
+        </div>
+
+        <div class="d-flex d-sm-none">
+            <b-row class="mx-0" style="width: 100%">
+                <b-col cols="12" class="d-flex justify-content-center">
+                    <b-navbar-brand>© 2022 Unnamed Item Studios</b-navbar-brand>
+                </b-col>
+                <b-col cols="12" class="d-flex justify-content-center">
+                    <b-icon variant="white-2" class="m-3" icon="instagram" scale="1.8"></b-icon>
+                    <b-icon variant="white-2" class="m-3" icon="youtube" scale="1.8"></b-icon>
+                    <b-icon variant="white-2" class="m-3" icon="twitter" scale="1.8"></b-icon>                    
+                </b-col>
+                <b-col cols="12" class="d-flex justify-content-center">
+                    <b-dropdown size="sm" variant="none" :text="$t('settings')" dropup>
+                        <b-dropdown-text>{{$t('languaje')}}</b-dropdown-text>
+
+                        <b-dropdown-divider></b-dropdown-divider>
+
+                        <b-dropdown-item @click="$i18n.locale = 'es'">
+                            <span><img src="@/assets/flags/es.png" width="30" height="18"> {{$t('spanish')}}</span>
+                        </b-dropdown-item>
+                        <b-dropdown-item @click="$i18n.locale = 'en'">
+                            <span><img src="@/assets/flags/uk.png" width="30" height="18"> {{$t('english')}}</span>
+                        </b-dropdown-item>
+
+                        <b-dropdown-divider></b-dropdown-divider>
+
+                        <b-dropdown-item @click="$theme.switch()">
+                            <b-icon :icon="$theme.color == $theme.dark ? 'moon' : 'sun'"></b-icon> {{$t($theme.color)}}
+                        </b-dropdown-item>
+                    </b-dropdown>
+                </b-col>
+            </b-row>
+        </div>
+    </div>
 </template>
 
 
--- src/components/TheNavbar.vue
+++ src/components/TheNavbar.vue
@@ -1,21 +1,47 @@
 <template>
     <b-navbar variant="dark" class="d-flex justify-content-center" sticky>
-        <b-nav>
-            <b-nav-item v-bind:class="[{'active': $route.name === 'Home'}]" to="/">Home</b-nav-item>
-            <b-nav-item >Projects</b-nav-item>
+        <b-nav class="d-none d-sm-flex">
+            <b-nav-item v-bind:class="[{'active': $route.name === 'Home'}]" to="/">{{$t('home')}}</b-nav-item>
+            <b-nav-item >{{$t('projects')}}</b-nav-item>
             <b-navbar-brand style="margin: auto">
                 <img src="@/assets/unnameditem_token.png" width="40" height="40">
             </b-navbar-brand>
-            <b-nav-item >Blog</b-nav-item>
-            <b-nav-item >Contact</b-nav-item>
+            <b-nav-item >{{$t('blog')}}</b-nav-item>
+            <b-nav-item >{{$t('contact')}}</b-nav-item>
+        </b-nav>
+
+        <b-nav class="d-flex d-sm-none justify-content-center">
+            <b-nav-item v-bind:class="[{'active': $route.name === 'Home'}]" to="/">{{$t('home')}}</b-nav-item>
+            <b-nav-item >{{$t('projects')}}</b-nav-item>
+            <b-nav-item >{{$t('blog')}}</b-nav-item>
+            <b-nav-item >{{$t('contact')}}</b-nav-item>
         </b-nav>
     </b-navbar>
 </template>
 
 
+<i18n>
+{
+    "en": {
+        "home": "Home",
+        "blog": "Blog",
+        "contact": "Contact",
+        "projects": "Projects"
+    },
+
+    "es": {
+        "home": "Inicio",
+        "blog": "Blog",
+        "contact": "Contacto",
+        "projects": "Projectos"
+    }
+}
+</i18n>
+
+
 <script>
 export default {
-    name: 'nav-bar'
+    name: 'nav-bar',
 }
 </script>
 
@@ -27,7 +53,7 @@ export default {
     }
 
     .nav-item {
-        min-width: 120px;
+        min-width: 130px;
         text-align: center;
         font-size: 25px;
         
