fc9630149c6c0d99c5b38f57c530c3f9913ee76e
Franco Cusmai <fcusmai@b2gov.com>
2022-07-13 16:35:57 -0300

    ig api grapgh

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 const express = require('express');
 const serveStatic = require('serve-static');
-// const ig = require('./ig.api');
+const ig = require('./ig.api');
 const path = require('path');
 const app = express();
 
@@ -12,20 +12,20 @@ app.get(/^((?!(\/ig$|\/ig\/)).)*$/, function (req, res) {
     res.sendFile(path.join(__dirname, '../dist/index.html'));
 })
 
-// app.get('/ig/media', async function(req, res) {
-//     var media = await ig.getMedia();
-//     res.json(JSON.parse(media));
-// })
+app.get('/ig/media', async function(req, res) {
+    var media = await ig.getMedia();
+    res.json(JSON.parse(media));
+})
 
-// app.get('/ig/profile', async function(req, res) {
-//     var profile = await ig.getProfile();
-//     res.json(JSON.parse(profile));
-// })
+app.get('/ig/profile', async function(req, res) {
+    var profile = await ig.getProfile();
+    res.json(JSON.parse(profile));
+})
 
-// app.get('/ig/media/:mediaId', async function(req, res) {
-//     var mediaContent = await ig.getMediaContent(req.params.mediaId);
-//     res.json(JSON.parse(mediaContent));
-// })
+app.get('/ig/media/:mediaId', async function(req, res) {
+    var mediaContent = await ig.getMediaContent(req.params.mediaId);
+    res.json(JSON.parse(mediaContent));
+})
 
 const port = process.env.PORT || 8080;
 app.listen(port);
